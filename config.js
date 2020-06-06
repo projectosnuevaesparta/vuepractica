d33e1f23b161d2f1c1b318c39c34577978f26998
CarlosHernandez1991 <51266679+CarlosHernandez1991@users.noreply.github.com>
2020-06-06 11:42:09 -0430

    config

---
+++ config.js
@@ -0,0 +1,14 @@
+
+
+export default{
+	appName: 'carlosMusic',
+	apiKey: '3354444a4c1b1bd580b0e3194aeef400',
+	secret: 'b900bd5117a31957eaa8238f36d5f74c'
+}
+
+/*
+Application name	carlosMusic
+API key	3354444a4c1b1bd580b0e3194aeef400
+Shared secret	b900bd5117a31957eaa8238f36d5f74c
+Registered to	thedaryn2010
+*/
